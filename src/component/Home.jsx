3e0d2884baecb335540405249729145cb2fc8dee
Sreedhar-roy <sreedharroy0@gmail.com>
2024-10-26 09:27:12 +0600

    javascript pure code commit in country name

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,17 +4,39 @@ import Countey from "./countey/Countey";
 
 const Home = () => {
 
+    const [visitedCountries, setVisitedcountries] = useState([]);
+
+    const handleVisited = (countries)=>{
+        console.log('add the visited countrires');
+        const newVisitedCountries = [...visitedCountries, countries];
+        setVisitedcountries(newVisitedCountries);
+    }
+
     const [counteys, setCounteys] = useState([]);
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCounteys(data))
+
+      
     },[])
     return (
         <div>
             <h2>Country :{counteys.length}</h2>
+            <div>
+            <h3>visited countries:{visitedCountries.length}</h3>
+            <ul>
+                {
+                    visitedCountries.map(country =><li key={country.cca3}>{country.name.common}</li>)
+                }
+            </ul>
+            </div>
             {
-                counteys.map(country => <Countey key={country.cca3} country={country}></Countey>)
+                counteys.map(country => <Countey 
+                    key={country.cca3} 
+                    handleVisited={handleVisited}
+                    country={country}>
+                    </Countey>)
             }
         </div>
     );
--- src/component/countey/Countey.jsx
+++ src/component/countey/Countey.jsx
@@ -1,10 +1,16 @@
 import './style.css'
-const Countey = ({country}) => {
-    const {name, flags} = country;
+const Countey = ({country, handleVisited}) => {
+    const {name, flags, population, area, cca3} = country;
+
+    console.log(handleVisited)
     return (
         <div className='design'>
             <p>name :{name?.common}</p>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>{area}</p>
+            <p>Code: {cca3}</p>
+            <button onClick={()=>handleVisited(country)}>Visited</button>
         </div>
     );
 };
